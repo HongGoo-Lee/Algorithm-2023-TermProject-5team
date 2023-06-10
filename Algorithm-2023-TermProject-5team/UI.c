e9fedeee1115d0524ee8180cded612bb9aba85b7
HongGoo-Lee <artin0307@gmail.com>
2023-06-11 01:10:05 +0900

    쓸모없는 테스트, UI 삭제

--- Algorithm-2023-TermProject-5team/UI.c
+++ Algorithm-2023-TermProject-5team/UI.c
@@ -9,349 +9,392 @@ void GotoXY(int x, int y) {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 }
 
-int test_ui(void) {
-
-    int i, category;
-    // 네모박스 안의 카테고리를 동적으로 설정하기 위한 배열을 설정 
-
-    char categories[9][20] = {
-        "전체",
-        "과자",
-        "음료수",
-        "간편식품",
-        "요리",
-        "아이스크림",
-        "생활잡화",
-        "컵라면",
-        "택배"
-    };
-
-    printf("%20s", "");
-    printf("┌");
-    for (i = 0; i < 50; i++) {
-        printf("─");
-    }
-    printf("┐\n");
-    printf("%20s", "");
-    printf("│%50s│\n", "");
-    printf("%20s", "");
-    printf("│%33s", "      	－O－ 편의점 프로그램－O－");
-    printf("%13s│", "");
-    printf("%20s", "\n");
-    printf("%20s", "");
-    printf("├");
-    for (i = 0; i < 50; i++) {
-        printf("─");
-    }
-    printf("┤\n");
-
-    printf("%20s", "");
-    printf("│");
-
-    printf("%5s", "");
-    printf("┌");
-    for (i = 0; i < 7; i++) {
-        printf("─");
-    }
-    printf("┬");
-    for (i = 0; i < 33; i++) {
-        printf("─");
-    }
-    printf("┐");
-
-    printf("%2s", "");
-
-    printf("│\n");
-    printf("%20s", "");
-    printf("│");
-    printf("%5s", "");
-    printf("│ 입  력  │");
-    printf("%33s", "");
-    printf("│");
-
-
-    printf("%2s", "");
-
-    printf("│\n");
-
-    printf("%20s", "");
-    printf("│");
-
-    printf("%5s", "");
-    printf("└");
-    for (i = 0; i < 7; i++) {
-        printf("─");
-    }
-    printf("┴");
-    for (i = 0; i < 33; i++) {
-        printf("─");
-    }
-    printf("┘");
-    printf("%2s", "");
-    printf("│\n");
-    printf("%20s", "");
-    printf("│");
-    printf("%50s", "");
-    printf("│\n");
-    printf("%20s", "");
-    printf("│");
-    printf("%4s", "");
-
-    printf("┌");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┐");
-
-    printf("%3s", "");
-
-    printf("┌");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┐");
-
-    printf("%3s", "");
-
-    printf("┌");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┐");
-    printf("%4s", "");
-    printf("│\n");
-
-    printf("%20s", "");
-    printf("│");
-
-
-    printf("%4s", "");
-    printf("│   %s", categories[0]);
-    printf("%3s", "");
-    printf("│");
-    printf("%3s", "");
-    printf("│   %s", categories[1]);
-    printf("%3s", "");
-    printf("│");
-    printf("%3s", "");
-    printf("│  %s", categories[2]);
-    printf("%2s", "");
-    printf("│");
-    printf("%4s", "");
-    printf("│\n");
-
-    printf("%20s", "");
-    printf("│");
-    printf("%4s", "");
-    printf("└");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┘");
-
-    printf("%3s", "");
-
-    printf("└");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┘");
-
-    printf("%3s", "");
-
-    printf("└");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┘");
-
-    printf("%4s", "");
-    printf("│\n");
-
-    printf("%20s", "");
-    printf("│");
-    printf("%50s", "");
-    printf("│\n");
-    printf("%20s", "");
-    printf("│");
-    printf("%4s", "");
-
-    printf("┌");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┐");
-
-    printf("%3s", "");
-
-    printf("┌");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┐");
-
-    printf("%3s", "");
-
-    printf("┌");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┐");
-    printf("%4s", "");
-    printf("│\n");
-
-    printf("%20s", "");
-    printf("│");
-
-
-    printf("%4s", "");
-    printf("│   %s", categories[3]);
-    printf("%1s", "");
-    printf("│");
-    printf("%3s", "");
-    printf("│   %s", categories[4]);
-    printf("%3s", "");
-    printf("│");
-    printf("%3s", "");
-    printf("│ %s", categories[5]);
-    printf("%s", "");
-    printf("│");
-    printf("%4s", "");
-    printf("│\n");
-
-
-    printf("%20s", "");
-    printf("│");
-    printf("%4s", "");
-    printf("└");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┘");
-
-    printf("%3s", "");
-
-    printf("└");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┘");
-
-    printf("%3s", "");
-
-    printf("└");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┘");
-
-    printf("%4s", "");
-    printf("│\n");
-
-    printf("%20s", "");
-    printf("│");
-    printf("%50s", "");
-    printf("│\n");
-    printf("%20s", "");
-    printf("│");
-    printf("%4s", "");
-
-    printf("┌");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┐");
-
-    printf("%3s", "");
-
-    printf("┌");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┐");
-
-    printf("%3s", "");
-
-    printf("┌");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┐");
-
-    printf("%4s", "");
-    printf("│\n");
-
-    printf("%20s", "");
-    printf("│");
-
-
-    printf("%4s", "");
-    printf("│   %s", categories[6]);
-    printf("%3s", "");
-    printf("│");
-    printf("%3s", "");
-    printf("│ %s", categories[7]);
-    printf("%3s", "");
-    printf("│");
-    printf("%3s", "");
-    printf("│ %s", categories[8]);
-    printf("%2s", "");
-    printf("│");
-    printf("%4s", "");
-    printf("│\n");
-
-    printf("%20s", "");
-    printf("│");
-    printf("%4s", "");
-    printf("└");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┘");
-
-    printf("%3s", "");
-
-    printf("└");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┘");
-
-    printf("%3s", "");
-
-    printf("└");
-    for (i = 0; i < 10; i++) {
-        printf("─");
-    }
-    printf("┘");
-
-    printf("%4s", "");
-    printf("│\n");
-
-    printf("%20s", "");
-    printf("│");
-    printf("%50s", "");
-    printf("│\n");
-    printf("%20s", "");
-    printf("│");
-    printf("%50s", "");
-    printf("│\n");
-    printf("%20s", "");
-    printf("│%50s│\n", "");
-    printf("%20s", "");
-    printf("└");
-    for (i = 0; i < 50; i++) {
-        printf("─");
-    }
-    printf("┘\n");
-
-
-
-    GotoXY(36, 5);
-    scanf_s("%d", &category);
-
-    system("cls");
-    print_items(category - 1);
-
-    return 0;
+void print_admin_menu();
+void stuff_menu();
+void location_menu();
+void road_menu();
+void sales_menu();
+void key_menu();
+void Rec();
+void resist();
+
+void print_admin_menu()
+{
+	int i, menu_num;
+
+	while (1) {
+		system("cls");
+		printf(" \n");
+		printf("%31s", "");
+		printf("<< 편의점 프로그램 관리자 모드 >>\n");
+		printf(" \n");
+		printf("%20s", "");
+		printf("┌");
+		for (i = 0; i < 50; i++)
+		{
+			printf("─");
+		}
+		printf("┐\n");
+
+		printf("%20s", "");
+		printf("│%50s│\n", "");
+		printf("%20s", "");
+		printf("│%30s", "1. 물건 관리");
+		printf("%20s│", "");
+
+		printf("\n%20s", "");
+		printf("│%50s│\n", "");
+		printf("%20s", "");
+		printf("│%30s", "2. 지역 관리");
+		printf("%20s│", "");
+
+		printf("\n%20s", "");
+		printf("│%50s│\n", "");
+		printf("%20s", "");
+		printf("│%30s", "3. 도로 관리");
+		printf("%20s│", "");
+
+		printf("\n%20s", "");
+		printf("│%50s│\n", "");
+		printf("%20s", "");
+		printf("│%35s", "4. 판매 내역 확인");
+		printf("%15s│", "");
+
+		printf("\n%20s", "");
+		printf("│%50s│\n", "");
+		printf("%20s", "");
+		printf("│%35s", "5. 관리자 키 변경");
+		printf("%15s│", "");
+
+		printf("\n%20s", "");
+		printf("│%50s│\n", "");
+		printf("%20s", "");
+		printf("└");
+		for (i = 0; i < 50; i++)
+		{
+			printf("─");
+		}
+		printf("┘\n");
+
+		printf("%20s", "");
+		printf("┌");
+		for (i = 0; i < 50; i++)
+		{
+			printf("─");
+		}
+		printf("┐\n");
+		printf("%20s", "");
+		printf("│%50s│\n", "");
+		printf("%20s", "");
+		printf("│%15s", "");
+		printf("입력:");
+		printf("%30s│", "");
+		printf("\n%20s", "");
+		printf("│%50s│", "");
+		printf("\n%20s", "");
+		printf("└");
+		for (i = 0; i < 50; i++)
+		{
+			printf("─");
+		}
+		printf("┘");
+		GotoXY(42, 18);
+		scanf("%d", &menu_num);
+
+		if (menu_num == 1) {
+			stuff_menu();
+		}
+		else if (menu_num == 2) {
+			location_menu();
+		}
+		else if (menu_num == 3) {
+			road_menu();
+		}
+		else if (menu_num == 4) {
+			sales_menu();
+		}
+		else if (menu_num == 5) {
+			key_menu();
+		}
+		else {
+			continue;
+		}
+	}
+}
+
+void stuff_menu() {
+	int i, menu_num;
+	system("cls");
+	printf(" \n");
+	printf("%38s", "");
+	printf("<< 물건 관리 >>\n");
+	printf(" \n");
+	printf("%20s", "");
+	printf("┌");
+	for (i = 0; i < 50; i++)
+	{
+		printf("─");
+	}
+	printf("┐\n");
+
+	printf("%20s", "");
+	printf("│%50s│\n", "");
+	printf("%20s", "");
+	printf("│%30s", "1. 물건 등록");
+	printf("%20s│", "");
+
+	printf("\n%20s", "");
+	printf("│%50s│\n", "");
+	printf("%20s", "");
+	printf("│%30s", "2. 물건 수정");
+	printf("%20s│", "");
+
+	printf("\n%20s", "");
+	printf("│%50s│\n", "");
+	printf("%20s", "");
+	printf("│%30s", "3. 물건 삭제");
+	printf("%20s│", "");
+
+
+	printf("\n%20s", "");
+	printf("│%50s│\n", "");
+	printf("%20s", "");
+	printf("└");
+	for (i = 0; i < 50; i++)
+	{
+		printf("─");
+	}
+	printf("┘\n");
+
+	printf("%20s", "");
+	printf("┌");
+	for (i = 0; i < 50; i++)
+	{
+		printf("─");
+	}
+	printf("┐\n");
+	printf("%20s", "");
+	printf("│%50s│\n", "");
+	printf("%20s", "");
+	printf("│%15s", "");
+	printf("입력:");
+	printf("%30s│", "");
+	printf("\n%20s", "");
+	printf("│%50s│", "");
+	printf("\n%20s", "");
+	printf("└");
+	for (i = 0; i < 50; i++)
+	{
+		printf("─");
+	}
+	printf("┘");
+	GotoXY(42, 14);
+	scanf("%d", &menu_num);
+
+	if (menu_num == 1) {
+		resist();
+	}
+}
+
+void location_menu() {
+	int i, menu_num;
+	system("cls");
+	printf(" \n");
+	printf("%38s", "");
+	printf("<< 지역 관리 >>\n");
+	printf(" \n");
+	printf("%20s", "");
+	printf("┌");
+	for (i = 0; i < 50; i++)
+	{
+		printf("─");
+	}
+	printf("┐\n");
+
+	printf("%20s", "");
+	printf("│%50s│\n", "");
+	printf("%20s", "");
+	printf("│%30s", "1. 지역 등록");
+	printf("%20s│", "");
+
+	printf("\n%20s", "");
+	printf("│%50s│\n", "");
+	printf("%20s", "");
+	printf("│%30s", "2. 지역 수정");
+	printf("%20s│", "");
+
+	printf("\n%20s", "");
+	printf("│%50s│\n", "");
+	printf("%20s", "");
+	printf("│%30s", "3. 지역 삭제");
+	printf("%20s│", "");
+
+
+	printf("\n%20s", "");
+	printf("│%50s│\n", "");
+	printf("%20s", "");
+	printf("└");
+	for (i = 0; i < 50; i++)
+	{
+		printf("─");
+	}
+	printf("┘\n");
+
+	printf("%20s", "");
+	printf("┌");
+	for (i = 0; i < 50; i++)
+	{
+		printf("─");
+	}
+	printf("┐\n");
+	printf("%20s", "");
+	printf("│%50s│\n", "");
+	printf("%20s", "");
+	printf("│%15s", "");
+	printf("입력:");
+	printf("%30s│", "");
+	printf("\n%20s", "");
+	printf("│%50s│", "");
+	printf("\n%20s", "");
+	printf("└");
+	for (i = 0; i < 50; i++)
+	{
+		printf("─");
+	}
+	printf("┘");
+	GotoXY(42, 14);
+	scanf("%d", &menu_num);
+}
+
+void road_menu() {
+	int i, menu_num;
+	system("cls");
+	printf(" \n");
+	printf("%38s", "");
+	printf("<< 도로 관리 >>\n");
+	printf(" \n");
+	printf("%20s", "");
+	printf("┌");
+	for (i = 0; i < 50; i++)
+	{
+		printf("─");
+	}
+	printf("┐\n");
+
+	printf("%20s", "");
+	printf("│%50s│\n", "");
+	printf("%20s", "");
+	printf("│%30s", "1. 도로 등록");
+	printf("%20s│", "");
+
+	printf("\n%20s", "");
+	printf("│%50s│\n", "");
+	printf("%20s", "");
+	printf("│%30s", "2. 도로 수정");
+	printf("%20s│", "");
+
+	printf("\n%20s", "");
+	printf("│%50s│\n", "");
+	printf("%20s", "");
+	printf("│%30s", "3. 도로 삭제");
+	printf("%20s│", "");
+
+
+	printf("\n%20s", "");
+	printf("│%50s│\n", "");
+	printf("%20s", "");
+	printf("└");
+	for (i = 0; i < 50; i++)
+	{
+		printf("─");
+	}
+	printf("┘\n");
+
+	printf("%20s", "");
+	printf("┌");
+	for (i = 0; i < 50; i++)
+	{
+		printf("─");
+	}
+	printf("┐\n");
+	printf("%20s", "");
+	printf("│%50s│\n", "");
+	printf("%20s", "");
+	printf("│%15s", "");
+	printf("입력:");
+	printf("%30s│", "");
+	printf("\n%20s", "");
+	printf("│%50s│", "");
+	printf("\n%20s", "");
+	printf("└");
+	for (i = 0; i < 50; i++)
+	{
+		printf("─");
+	}
+	printf("┘");
+	GotoXY(42, 14);
+	scanf("%d", &menu_num);
+}
+
+void sales_menu() {
+
+
+}
+
+void key_menu() {
+
+}
+
+void resist() {
+	char category[20];
+	char stuff_name[20];
+	char price[20];
+
+	system("cls");
+	printf(" \n");
+	printf("%41s<< 등록 >>\n", "");
+	Rec();
+	GotoXY(30, 4);
+	printf("카테고리: ");
+	scanf("%s", category);
+	system("cls");
+	printf(" \n");
+	printf("%41s<< 등록 >>\n", "");
+	Rec();
+	GotoXY(30, 4);
+	printf("상품이름: ");
+	scanf("%s", stuff_name);
+	system("cls");
+	printf(" \n");
+	printf("%41s<< 등록 >>\n", "");
+	Rec();
+	GotoXY(30, 4);
+	printf("가격: ");
+	scanf("%s", price);
+
+
+}
+
+void Rec() {
+	int i;
+	printf("%20s", "");
+	printf("┌");
+	for (i = 0; i < 50; i++)
+	{
+		printf("─");
+	}
+	printf("┐\n");
+	printf("%20s", "");
+	printf("│%50s│\n", "");
+	printf("%20s", "");
+	printf("│%50s│", "");
+	printf("\n%20s", "");
+	printf("│%50s│", "");
+	printf("\n%20s", "");
+	printf("└");
+	for (i = 0; i < 50; i++)
+	{
+		printf("─");
+	}
+	printf("┘");
 }
